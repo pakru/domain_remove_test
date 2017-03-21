94c50deb66c9ed3b6233a9b25105363f1f614b3e
Pavel Krutikhin <pa_kru@ngs.ru>
2017-03-21 09:44:50 +0700

    Some optimisations modified logging path

--- dom-remove-test.py
+++ dom-remove-test.py
@@ -1,44 +1,19 @@
 #!/usr/local/bin/python3.5
 
-import config
+import config, time, sys, colorama, logging
 #import os
-import time
-import sys
-import colorama
 from colorama import Fore, Back, Style
 #import modules.cocon_interface as ccn_iface
 #import threading
-import logging
 #import json
 #import argparse
 import ssh_cocon.ssh_cocon as ccn
 #import argparse
 # JSON parsing
-'''
-testConfigFile = open('dom-remove.json')
-config.testConfigJson = json.loads(testConfigFile.read())
-testConfigFile.close()
-'''
-'''
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--custom_config', type=argparse.FileType())
-#parser.add_argument("-с", "--config", help="path to *.json config file")
-args = parser.parse_args()
-if args.custom_config:
-    print("config is used: " + str(args.custom_config))
-'''
 
 testingDomain = config.testConfigJson['DomainName']
-#logFile = config.logPath+'/'+config.testConfigJson['TestScript'] + '.log'
-'''
-coreNode = 'core1@ecss1'
-sipNode = 'sip1@ecss1'
-dsNode = 'ds1@ecss1'
-'''
 
 colorama.init(autoreset=True)
-#print('Logging to: ' + logFile)
-#logging.basicConfig(filename='example.log', filemode='w', format = u'%(asctime)-8s %(levelname)s [%(module)s -> %(funcName)s:%(lineno)d] %(message)-8s', level = logging.INFO)
 
 def preconfigure():
     if ccn.domainDeclare(dom=testingDomain,removeIfExists = True):
@@ -95,47 +70,6 @@ def preconfigure():
         logging.info('Domain ' + testingDomain + ' is not exists in cocon')
     return True
 
-
-# def ccn_iface_preconfigure():
-
-
-# logging.basicConfig(format = u'%(asctime)-8s %(levelname)-8s %(message)-8s', filemode='w', level = logging.INFO)
-# logger = logging.getLogger("tester")
-'''
-test_var = {"%%DEV_USER%%":"admin", "%%DEV_PASS%%":"password", "%%SERV_IP%%":"192.168.118.49"}
-coconInt = ccn_iface.coconInterface(test_var, show_cocon_output=True)
-coconInt.eventForStop = threading.Event()
-#Поднимаем thread
-coconInt.myThread = threading.Thread(target=ccn_iface.ccn_command_handler, args=(coconInt,))
-coconInt.myThread.start()
-#Проверяем, что он жив.
-time.sleep(0.2)
-if not coconInt.myThread.is_alive():
-	print('Can\'t start CCN configure thread')
-	sys.exit(1)
-
-test = '/node/uptime\nsystem-status\n'
-
-# ccn_iface.cocon_push_string_command(coconCommands,coconInt)
-
-ccn_iface.cocon_push_string_command(coconCommands=test, coconInt = coconInt)
-
-print('recieved: ')
-print(coconInt.data.decode('utf-8'))
-'''
-
-'''
-testConfigFile = open('dom-remove.json')
-testConfigStr = testConfigFile.read()
-testConfigJson = json.loads(testConfigStr)
-'''
-# print('Readed json: ')
-# print(testConfigJson)
-
-# print(testingDomain)
-
-
-
 print(Fore.LIGHTWHITE_EX +'-Start domain remove test-')
 logging.info('Start domain remove test')
 if not preconfigure():
@@ -150,7 +84,5 @@ else:
 
 print(Fore.GREEN + 'It seems to be all FINE...')
 print('We did it!!')
-
-#ccn.coconInt.eventForStop.set()
 
 sys.exit(0)
